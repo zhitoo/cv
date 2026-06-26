779f82efa01168bb3817ba1a56a71e8a69d64b67
hossein <hshafiei374@gmail.com>
2026-06-26 13:42:00 +0330

    fix number

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -20,7 +20,7 @@ export default function Sidebar() {
         <ContactItem
           icon={<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />}
           href="tel:09114151950"
-          text="09114154950"
+          text="09114151950"
         />
         <ContactItem
           icon={<><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></>}
